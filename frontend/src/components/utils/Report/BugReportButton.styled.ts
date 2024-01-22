dd52343a946d3f3c0e38876dc5d352c2f7e63ce7
ping9oo <fpdl96@gmail.com>
2024-01-22 22:13:10 +0900

    Feat: 신고 및 건의 기능 수정
    css를 조정하였습니다.
    엔터키를 누르면 페이지가 넘어가는 현상을 수정하였습니다.
    
    Resolves: #36

--- frontend/src/components/utils/Report/BugReportButton.styled.ts
+++ frontend/src/components/utils/Report/BugReportButton.styled.ts
@@ -20,22 +20,28 @@ export const ReportButtonDiv = styled.div`
 
 export const ModalDiv = styled.div`
   width: 25vw;
-  height: 55vh;
-  flex-shrink: 0;
+  height: 50vh;
   border: solid 5px rgba(235, 226, 255, 0.4);
   border-radius: 30px;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: rgba(255, 118, 120, 1);
-  position: absolute;
+  background-color: rgba(46, 178, 234, 1);
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   z-index: 1;
 `;
 
 export const StyledContents = styled.textarea`
   width: 20vw;
-  height: 10vh;
+  height: 19vh;
+  padding: 15px;
+  border: 5px solid rgba(200, 191, 191, 0.7);
+  border-radius: 8px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 `;
 
 export const ModalOverlay = styled.div`
@@ -59,6 +65,46 @@ export const CloseButton = styled.button`
 
 export const CheckBoxContainer = styled.div`
   display: flex;
-  justify-content: space-around; 
-  margin-bottom: 5px; 
+  justify-content: space-around;
+  margin-bottom: 5px;
+  flex-direction: row;
+  margin: 20px 0;
+  label {
+    margin-right: 20px;
+    font-weight: bold;
+
+    &:last-child {
+      margin-right: 0;
+    }
+    input[type='radio'] {
+      transform: scale(1.5); 
+    }
+  }
+`;
+
+export const StyledReportSubmitButton = styled.button`
+  width: 114px;
+  height: 65px;
+  flex-shrink: 0;
+  border-radius: 45px;
+  border: 5px solid #f3ce6d;
+
+  background: #f9c10c;
+
+  box-shadow:
+    0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset,
+    0px 4px 4px 0px rgba(179, 179, 179, 0.25);
+  text-align: center;
+  font-family: Galmuri11;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  color: white;
+  margin-top: 30px;
+`;
+
+export const ReportTitleDiv = styled.div`
+  font-size: 30px;
+  font-weight: 1000;
 `;
