ecd6e006d4053ffab91480a87ce88fd28e4859af
CHAERYEON-LEE <sogogijjang@naver.com>
2024-01-26 13:44:17 +0900

    Feat: 공지 팝업 컴포넌트  구현 완료
    로컬스토리지를 사용한 팝업 컴포넌트 구현 완료
    - 기존에 한번도 팝업을 보지 못한 유저는 랜딩페이지에서 띄워준다
    - 팝업을 확인하고 팝업창 밑의 다시보지않기 를 체크하고나서 팝업을 닫으면
      그 이후는 재방문해도 팝업창이 뜨지 않는다
    - 다시보지않기 버튼을 클릭하지 않으면 재방문했을 때 팝업이 다시 뜨게
      된다
    
    Resolves: #49

--- frontend/src/pages/Landing/Landing.styled.ts
+++ frontend/src/pages/Landing/Landing.styled.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+`;
+
 export const LandingPageContainer = styled.div`
   position: relative;
   width: 100vw;
